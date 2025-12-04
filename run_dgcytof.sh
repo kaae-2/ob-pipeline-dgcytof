a754814cc1eda22746aac3350514e630048ea8af
Frederik Kirk <kaae-2@hotmail.com>
2025-12-04 15:55:22 +0100

    updated bash

--- run_dgcytof.sh
+++ run_dgcytof.sh
@@ -3,11 +3,11 @@ set -euo pipefail
 
 # Run data_preprocessing.py with the requested parameters.
 script_dir="$(cd -- "$(dirname -- "$0")" && pwd)"
-python_bin="${script_dir}/.venv/bin/python"
-[ -x "$python_bin" ] || python_bin="python"
 
-"${python_bin}" "${script_dir}/dgcytof_cli.py" \
+python "${script_dir}/dgcytof_cli.py" \
   --name "dgcytof" \
   --output_dir "${script_dir}/out/data/analysis/default/dgcytof" \
-  --data.matrix "${script_dir}/out/data/data_preprocessing/default/data_preprocessing.csv" \
-  --data.true_labels "${script_dir}/out/data/data_preprocessing/default/data_preprocessing_labels.txt"
+  --data.matrix "${script_dir}/out/data/data_preprocessing/default/data_import.matrix.gz" \
+  --data.matrix.train "${script_dir}/out/data/data_preprocessing/default/data_import.matrix.training.gz" \
+  --data.true_labels "${script_dir}/out/data/data_preprocessing/default/data_import.true_labels.gz" \
+  --data.true_labels.train "${script_dir}/out/data/data_preprocessing/default/data_import.true_labels.gz"
